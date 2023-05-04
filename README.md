2fefcd889e67786a77d1fbad0aaaa748caf4f43b
Sergij Suprunenko <104130197+Soteach@users.noreply.github.com>
2023-05-04 12:54:58 +0300

    make CLI

---
+++ README.md
@@ -0,0 +1,16 @@
+Виведення списку контактів у вигляді таблиці:
+
+Команда: node index.js --action="list"
+Посилання на скріншот: https://ibb.co/3fgv0Dg
+
+Отримання контакту по ID:
+Команда: node index.js --action="get" --id 05olLMgyVQdWRwgKfg5J6
+Посилання на скріншот: https://ibb.co/CWrM4d1
+
+Додавання контакту:
+Команда:node index.js --action="add" --name Mango --email mango@gmail.com --phone 322-22-22
+Посилання на скріншот: https://ibb.co/3zKywgp
+
+Видалення контакту:
+Команда:node index.js --action="remove" --id drsAJ4SHPYqZeG-83QTVW
+Посилання на скріншот: https://ibb.co/LkhzTXF
---
+++ contacts.js
@@ -0,0 +1,51 @@
+const fs = require('fs').promises;
+const path = require('path');
+const { nanoid } = require('nanoid');
+
+
+const contactsPath = path.join(__dirname, "./db/contacts.json");
+
+async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+}
+
+async function getContactById(contactId) {
+    const data = await listContacts();
+    const contactById = data.find(contact => contact.id === contactId);
+    if (!contactById) {
+        console.log("No contacts with such ID");
+        return null;
+    }
+    return contactById||null;
+}
+
+async function removeContact(contactId) {
+    const data = await listContacts();
+    const index = data.findIndex((contact) => contact.id === contactId);
+    if (index === -1) {
+        console.log("No contact with such id");
+        return null;
+    }
+    const deletedContact = data.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(data, null, 2));
+  return deletedContact;
+}
+
+
+
+async function addContact(name, email, phone) {
+    const data = await listContacts();
+    const newContact = { id: nanoid(), name, email, phone };
+    data.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(data, null, 2));
+    return newContact;
+}
+
+module.exports = {
+    contactsPath,
+    listContacts,
+    getContactById,
+    addContact,
+    removeContact,
+};
