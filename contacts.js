0ac941afa8058d0bb1499a256fe599f1eec89f4a
Sergij Suprunenko <104130197+Soteach@users.noreply.github.com>
2023-05-05 13:32:04 +0300

    fix error according mentor remark

--- contacts.js
+++ contacts.js
@@ -6,8 +6,13 @@ const { nanoid } = require('nanoid');
 const contactsPath = path.join(__dirname, "./db/contacts.json");
 
 async function listContacts() {
-  const data = await fs.readFile(contactsPath);
-  return JSON.parse(data);
+    let data = [];
+    try {
+        data = JSON.parse(await fs.readFile(contactsPath));
+    } catch (error) {
+        console.log("Can't read Contacts!");
+    }
+   return data;
 }
 
 async function getContactById(contactId) {
